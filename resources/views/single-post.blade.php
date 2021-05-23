8a08d9d4d7fcb76de134b279d3296ae50e45370e
RusiraPathum <48381430+RusiraPathum@users.noreply.github.com>
2021-05-24 01:10:52 +0530

    Single Post View Part

--- resources/views/single-post.blade.php
+++ resources/views/single-post.blade.php
@@ -1,1 +1,47 @@
 <?php
+use Illuminate\Support\Facades\Session;
+?>
+    <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post Details</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+<body>
+
+<section class="container mt-5">
+
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Post Details</h1>
+                </div>
+                <div class="card-body">
+                    <h5>{{$post->title}}</h5>
+                    <p>{{$post->body}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
